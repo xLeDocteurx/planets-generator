4aa95819280564b13c32c865218301b50bf1ca14
xLeDocteurx <lenoirc.fr@gmail.com>
2019-09-20 13:43:25 +0200

    working on the spherical algorythm

--- public/js/pg/Cube.js
+++ public/js/pg/Cube.js
@@ -42,6 +42,11 @@ class Face {
 		this.verteces = [];
 	}
 
+	// TO DO ( éviter la répétition dans la fonction init() )
+	static makeVertecesForAFace(face_index,size,resolution){
+
+	}
+
 	init(face_index, size, resolution){
 
 		const v1 = createVector(0,0,0);
@@ -65,8 +70,17 @@ class Face {
 						const a1A = createVector(0,0,0);
 						const a2A = createVector(0,0,originVertex.z);
 						const adjacentA = a1A.dist(a2A);
-						const angleA = Math.atan(oppositeA / adjacentA);
-					    this.verteces.push({angle:angleA,x:ax,y:ay,z:az});
+						const h1A = createVector(0,0,0);
+						const h2A = createVector(ax,ay,az);
+						const hypoA= h1A.dist(h2A);
+						const angleA = Math.atan(oppositeA/adjacentA);
+
+						const newAZ = cos(angleA)*farestVertex;
+						const newAY = sin(angleA)*farestVertex;
+						const zAxisAdditionA = (cos(angleA)*farestVertex)-(cos(angleA)*hypoA);
+						const yAxisAdditionA = (sin(angleA)*farestVertex)-(sin(angleA)*hypoA);
+
+					    this.verteces.push({hypo:hypoA,adjacent:adjacentA,opposite:oppositeA,angle:angleA,x:ax/*+yAxisAdditionA*/,y:ay/*+yAxisAdditionA*/,z:newAZ});
 
 			    		const bx = originVertex.x+(x+1)*sliceSize;
 			    		const by = originVertex.y+(y+1)*sliceSize;
@@ -77,8 +91,17 @@ class Face {
 						const a1B = createVector(0,0,0);
 						const a2B = createVector(0,0,originVertex.z);
 						const adjacentB = a1B.dist(a2B);
-						const angleB = Math.atan(oppositeB / adjacentB);
-					    this.verteces.push({angle:angleB,x:bx,y:by,z:bz});
+						const h1B = createVector(0,0,0);
+						const h2B = createVector(bx,by,bz);
+						const hypoB= h1B.dist(h2B);
+						const angleB = Math.atan(oppositeB/adjacentB);
+
+						const newBZ = cos(angleB)*farestVertex;
+						const newBY = sin(angleB)*farestVertex;
+						const zAxisAdditionB = (cos(angleB)*farestVertex)-(cos(angleB)*hypoB);
+						const yAxisAdditionB = (sin(angleB)*farestVertex)-(sin(angleB)*hypoB);
+
+					    this.verteces.push({hypo:hypoB,adjacent:adjacentB,opposite:oppositeB,angle:angleB,x:bx/*+yAxisAdditionB*/,y:by/*+yAxisAdditionB*/,z:newBZ});
 
 			    		const cx = originVertex.x+x*sliceSize;
 			    		const cy = originVertex.y+(y+1)*sliceSize;
@@ -89,8 +112,17 @@ class Face {
 						const a1C = createVector(0,0,0);
 						const a2C = createVector(0,0,originVertex.z);
 						const adjacentC = a1C.dist(a2C);
-						const angleC = Math.atan(oppositeC / adjacentC);
-					    this.verteces.push({angle:angleC,x:cx,y:cy,z:cz});
+						const h1C = createVector(0,0,0);
+						const h2C = createVector(cx,cy,cz);
+						const hypoC= h1C.dist(h2C);
+						const angleC = Math.atan(oppositeC/adjacentC);
+
+						const newCZ = cos(angleC)*farestVertex;
+						const newCY = sin(angleC)*farestVertex;
+						const zAxisAdditionC = (cos(angleC)*farestVertex)-(cos(angleC)*hypoC);
+						const yAxisAdditionC = (sin(angleC)*farestVertex)-(sin(angleC)*hypoC);
+
+					    this.verteces.push({hypo:hypoC,adjacent:adjacentC,opposite:oppositeC,angle:angleC,x:cx/*+yAxisAdditionC*/,y:cy/*+yAxisAdditionC*/,z:newCZ});
 					    
 			    		const dx = originVertex.x+x*sliceSize;
 			    		const dy = originVertex.y+y*sliceSize;
@@ -101,8 +133,17 @@ class Face {
 						const a1D = createVector(0,0,0);
 						const a2D = createVector(0,0,originVertex.z);
 						const adjacentD = a1D.dist(a2D);
-						const angleD = Math.atan(oppositeD / adjacentD);
-					    this.verteces.push({angle:angleD,x:dx,y:dy,z:dz});
+						const h1D = createVector(0,0,0);
+						const h2D = createVector(dx,dy,dz);
+						const hypoD= h1D.dist(h2D);
+						const angleD = Math.atan(oppositeD/adjacentD);
+
+						const newDZ = cos(angleD)*farestVertex;
+						const newDY = sin(angleD)*farestVertex;
+						const zAxisAdditionD = (cos(angleD)*farestVertex)-(cos(angleD)*hypoD);
+						const yAxisAdditionD = (sin(angleD)*farestVertex)-(sin(angleD)*hypoD);
+
+					    this.verteces.push({hypo:hypoD,adjacent:adjacentD,opposite:oppositeD,angle:angleD,x:dx/*+yAxisAdditionD*/,y:dy/*+yAxisAdditionD*/,z:newDZ});
 					    
 			    		const ex = originVertex.x+(x+1)*sliceSize;
 			    		const ey = originVertex.y+y*sliceSize;
@@ -113,8 +154,17 @@ class Face {
 						const a1E = createVector(0,0,0);
 						const a2E = createVector(0,0,originVertex.z);
 						const adjacentE = a1E.dist(a2E);
-						const angleE = Math.atan(oppositeE / adjacentE);
-					    this.verteces.push({angle:angleE,x:ex,y:ey,z:ez});
+						const h1E = createVector(0,0,0);
+						const h2E = createVector(ex,ey,ez);
+						const hypoE= h1E.dist(h2E);
+						const angleE = Math.atan(oppositeE/adjacentE);
+
+						const newEZ = cos(angleE)*farestVertex;
+						const newEY = sin(angleE)*farestVertex;
+						const zAxisAdditionE = (cos(angleE)*farestVertex)-(cos(angleE)*hypoE);
+						const yAxisAdditionE = (sin(angleE)*farestVertex)-(sin(angleE)*hypoE);
+
+					    this.verteces.push({hypo:hypoE,adjacent:adjacentE,opposite:oppositeE,angle:angleE,x:ex/*+yAxisAdditionE*/,y:ey/*+yAxisAdditionE*/,z:newEZ});
 					    
 			    		const fx = originVertex.x+(x+1)*sliceSize;
 			    		const fy = originVertex.y+(y+1)*sliceSize;
@@ -125,8 +175,17 @@ class Face {
 						const a1F = createVector(0,0,0);
 						const a2F = createVector(0,0,originVertex.z);
 						const adjacentF = a1F.dist(a2F);
-						const angleF = Math.atan(oppositeF / adjacentF);
-					    this.verteces.push({angle:angleF,x:fx,y:fy,z:fz});
+						const h1F = createVector(0,0,0);
+						const h2F = createVector(fx,fy,fz);
+						const hypoF= h1F.dist(h2F);
+						const angleF = Math.atan(oppositeF/adjacentF);
+
+						const newFZ = cos(angleF)*farestVertex;
+						const newFY = sin(angleF)*farestVertex;
+						const zAxisAdditionF = (cos(angleF)*farestVertex)-(cos(angleF)*hypoF);
+						const yAxisAdditionF = (sin(angleF)*farestVertex)-(sin(angleF)*hypoF);
+
+					    this.verteces.push({hypo:hypoF,adjacent:adjacentF,opposite:oppositeF,angle:angleF,x:fx/*+yAxisAdditionF*/,y:fy/*+yAxisAdditionF*/,z:newFZ});
 				}
 			}
   		} else if(face_index == 1) {
