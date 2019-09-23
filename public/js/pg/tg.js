788dfcd848d67580d758cc75a44163a9a5b17921
xLeDocteurx <lenoirc.fr@gmail.com>
2019-09-23 18:08:49 +0200

    noise scale, octave and falloff integrated

--- public/js/pg/tg.js
+++ public/js/pg/tg.js
@@ -18,8 +18,8 @@ let options = {
 		size: 300,
 		resolution: 24,
 		showWater: true,
-		// waterLevel: 0.56,
-		waterLevel: 1,
+		waterLevel: 0.56,
+		// waterLevel: 1,
 	},
 	noise_beta: {
 		seed: "seed",
@@ -27,8 +27,8 @@ let options = {
 		offset: 300,
 		scale: 0.15,
 		octave: 4,
-		// falloff: 0.5,
-		falloff: 1,
+		falloff: 0.5,
+		// falloff: 1,
 		strength: 1.0,
 	},
 	// stop: function() {
