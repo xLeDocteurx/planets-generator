1320ab4b9b129fe4d05335909612d539c57512bf
xLeDocteurx <lenoirc.fr@gmail.com>
2019-09-24 18:38:51 +0200

    - Fancy new skybox

--- public/js/pg/Skybox.js
+++ public/js/pg/Skybox.js
@@ -8,12 +8,12 @@ class Skybox {
 
 		const skyboxGeometry = new THREE.CubeGeometry(planeteSize*100, planeteSize*100, planeteSize*100);
 		const skyboxAssetsMaterial = [
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/NegativeZ.png"), side: THREE.DoubleSide}),
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/PositiveZ.png"), side: THREE.DoubleSide}),
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/NegativeY.png"), side: THREE.DoubleSide}),
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/PositiveY.png"), side: THREE.DoubleSide}),
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/NegativeX.png"), side: THREE.DoubleSide}),
-			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/milkyway/PositiveX.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/right.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/left.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/top.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/bot.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/front.png"), side: THREE.DoubleSide}),
+			new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("assets/sf-lightblue/back.png"), side: THREE.DoubleSide}),
 		];
 		// const skyboxMaterial = new THREE.MeshFaceMaterial(skyboxAssetsMaterial);
 
