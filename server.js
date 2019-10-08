41325c25511faba841873d6fc2236b9f70e5ab5a
xLeDocteurx <lenoirc.fr@gmail.com>
2019-10-08 22:49:03 +0200

    added n 'arx' route to debug faster than with 'ar' route

--- server.js
+++ server.js
@@ -91,6 +91,53 @@ app.post("/ar", (req, res) => {
 	});
 });
 
+// app.get("/ar/:id", (req, res) => {
+app.get("/arx", (req, res) => {
+	// const sess = req.session;
+
+	const options = {
+		space: {
+			ambientLight: 0.0,
+			// ambientLight: 1.0,
+			directionnalLight: 0.3,
+			// pointLights: 1.0,
+			pointLights: 0.005,
+		},
+		planete: {
+			size: 1,
+			resolution: 24,
+			// resolution: 8,
+			showWater: true,
+			// showWater: false,
+			// abyssesLevel: 0.40,
+			abyssesLevel: 0.33,
+			// waterLevel: 0.56,
+			waterLevel: 0.50,
+			groundLevel: 1,
+			// waterLevel: 1,
+		},
+		noise_beta: {
+			seed: "seed",
+			// scale: 0.12,
+			offset: 300,
+			scale: 0.15,
+			// scale: 0.015,
+			octave: 4,
+			// octave: 1,
+			falloff: 0.5,
+			// falloff: 1,
+			// strength: 0.25,
+			// strength: 1,
+			strength: 0.4,
+		},
+	};
+
+	res.render("arx", {
+		options: options,
+		// posts: data.posts
+	});
+});
+
 io.on('connection', (socket) => {
   console.log("--- // Nouvelle connection");
   console.log("Connection ID : " + socket.id);
